9bdc89cc319b925ac382e212cd635f8e6a127e0f
ZhivaldoFabio <zhivaldo.fabio@ti.ukdw.ac.id>
2025-09-19 18:34:18 +0700

    cleaning up
    deleting wuzznet & adding kantin-joglo

--- README.md
+++
@@ -1,2 +0,0 @@
-# zhivaldofabio.github.io
-
--- src/pages/Login.tsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Login Page</h1>
-    </div>
-  )
-}
